5ebbd52a93da69c17e174494c8b41bcab0c5943d
BoburDev <86083338+RayimjonovBobur@users.noreply.github.com>
2022-03-29 21:04:59 +0500

    Online Shop

---
+++ backend/products.js
@@ -0,0 +1,36 @@
+const products = [
+  {
+    id: 1,
+    name: "iPhone 12 Pro Max",
+    desc: "7.1-inch display",
+    price: 1300,
+    image:
+      "https://res.cloudinary.com/imsoft/image/upload/v1642595424/OnlineShop/iphone12pro_cloaj0.jpg",
+  },
+  {
+    id: 2,
+    name: "iPhone 12 Pro",
+    desc: "6.1-inch display",
+    price: 850,
+    image:
+      "https://res.cloudinary.com/imsoft/image/upload/v1642595424/OnlineShop/iphone12_rba8wh.jpg",
+  },
+  {
+    id: 3,
+    name: "Vivo 21S",
+    desc: "6.2-inch display",
+    price: 799,
+    image:
+      "https://res.cloudinary.com/imsoft/image/upload/v1642595424/OnlineShop/galaxyS_lee7se.png",
+  },
+  // {
+  //   id: 3,
+  //   name: "Iphone 13 Pro Max",
+  //   desc: "10.2-inch display",
+  //   price: 1409,
+  //   image:
+  //     "https://res.cloudinary.com/imsoft/image/upload/v1642671952/OnlineShop/iphone13_aiczpf.jpg",
+  // },
+];
+
+module.exports = products;
